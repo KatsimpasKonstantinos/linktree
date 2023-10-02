5b1f8d971ffd8ce8f8b806ca190d6ac583fd5fd7
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2023-10-02 14:10:40 +0200

    rain

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    rain();
+  }, []);
+
+  var rainDensity = 1;
+  var duration = 1;
+
+  function rain() {
+    var rainDiv = document.getElementById("rain");
+    for (var i = 0; i < 100; i += (Math.random() / rainDensity) * (1500 / window.innerWidth)) {
+      var span = '<div class="raindrop" style="left:' + i * 1 + '%;animation-delay: ' + Math.random() + 's;' +
+        'animation-duration: ' + (Math.random() * 2 + 0.5) * duration + 's; bottom: ' + (100) + '%;"></div>';
+      if (rainDiv) rainDiv.innerHTML += span;
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,8 +34,11 @@ function App() {
           LinkedIn
         </a>
       </header>
+      <div id="rain"></div>
     </div>
   );
 }
+
+
 
 export default App;
